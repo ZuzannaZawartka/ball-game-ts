6e22098161ac4a2e61a2de5f401fc80c3a4350cd
Zuzia <raf.zuzia7@gmail.com>
2022-09-25 16:57:12 +0200

    feat: added choosing start/end

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
+// interface startEnd {
+//     start: number | null,
+//     end: number | null;
+// }
 var Square = /** @class */ (function () {
-    function Square(theID, theSize, theText) {
-        this.checked = false;
+    function Square(theID, theSize, theText, theCordX, theCordY) {
+        this.isChecked = false;
+        this.isObstacle = false;
         this.id = theID;
         this.size = theSize;
-        this.text = theText;
+        this.text = (theText).toString();
+        this.cords = { x: theCordX, y: theCordY };
     }
     Square.prototype.create = function () {
         var element = document.createElement("div");
-        element.classList.add("punkt");
+        element.id = (this.id).toString();
+        element.classList.add("point");
         element.innerText = (this.text).toString();
         element.setAttribute('style', "width: ".concat(this.size, "px; height: ").concat(this.size, "px; border:1px solid black"));
         return element;
+    };
+    Square.prototype.checkIsAvailable = function () {
+        var element = document.getElementById(this.id.toString());
+        if (element != (undefined || null))
+            return element;
+        else
+            return null;
+    };
+    Square.prototype.setObstacle = function () {
+        var obstacleElem = this.checkIsAvailable();
+        if (obstacleElem) {
+            this.isObstacle = true;
+            this.text = "X";
+            obstacleElem.innerHTML = this.text;
+        }
+    };
+    Square.prototype.setAsStartEnd = function (name) {
+        var obstacleElem = this.checkIsAvailable();
+        if (obstacleElem) {
+            if (name == "START") {
+                this.text = "START";
+            }
+            else {
+                this.text = "END";
+            }
+            obstacleElem.style.backgroundColor = "red";
+            obstacleElem.innerHTML = this.text;
+        }
     };
     return Square;
 }());
@@ -18,30 +53,48 @@ var SquareList = /** @class */ (function () {
     function SquareList(theQuantity, theSizeOfSquare) {
         this.quantity = theQuantity;
         this.sizeOfSquare = theSizeOfSquare;
+        this.startEndObject = [];
         this.squareList = [];
-        // console.log(this.squareList)
-        // let s = new Square(0, 12)
-        // this.squareList.push({ x: 2, y: 3, object: s })
         this.makeBoard();
+        this.addListener();
     }
     SquareList.prototype.makeBoard = function () {
         var _a;
+        var count = 0;
         for (var i = 0; i < this.quantity; i++) {
             for (var j = 0; j < this.quantity; j++) {
-                var square = new Square(i, this.sizeOfSquare, 0);
-                this.squareList.push({ x: j, y: i, object: square });
+                var square = new Square(count++, this.sizeOfSquare, 0, i, j);
+                this.squareList.push(square);
                 (_a = document.getElementById('root')) === null || _a === void 0 ? void 0 : _a.appendChild(square.create());
             }
         }
     };
-    // refreshBoard() {
-    //     document.getElementById("root")?.innerHTML = ""
-    // }
+    SquareList.prototype.addListener = function () {
+        var _this = this;
+        document.addEventListener('click', function (e) {
+            var _a, _b;
+            if (e.target.classList.contains('point') && _this.startEndObject.length < 2) {
+                var id_1 = parseInt(e.target.id);
+                if (_this.startEndObject.length < 1)
+                    (_a = _this.squareList.find(function (element) { return element.id == id_1; })) === null || _a === void 0 ? void 0 : _a.setAsStartEnd("START");
+                else
+                    (_b = _this.squareList.find(function (element) { return element.id == id_1; })) === null || _b === void 0 ? void 0 : _b.setAsStartEnd("END");
+                _this.startEndObject.push(id_1);
+                console.log(_this.startEndObject);
+            }
+        });
+    };
     SquareList.prototype.generateObstacles = function (numberOfObstacle) {
-        var x = ["zuzia", "kasia", "asia", "basak"];
+        var _a;
+        var _loop_1 = function (i) {
+            var x = generateRandomInteger(0, 4);
+            var y = generateRandomInteger(0, 4);
+            (_a = this_1.squareList.find(function (element) { return element.cords.x == x && element.cords.y == y; })) === null || _a === void 0 ? void 0 : _a.setObstacle();
+            console.log(this_1.squareList.find(function (element) { return element.cords.x == x && element.cords.y == y; }));
+        };
+        var this_1 = this;
         for (var i = 0; i < numberOfObstacle; i++) {
-            console.log("TO:");
-            console.log(x.find(function (el) { return el == "zuzia"; }));
+            _loop_1(i);
         }
     };
     return SquareList;
--- script.ts
+++ script.ts
@@ -1,65 +1,124 @@
+interface cordsx {
+    x: number,
+    y: number
+}
+
+// interface startEnd {
+//     start: number | null,
+//     end: number | null;
+// }
 class Square {
     id: number;
     size: number;
-    checked: boolean = false;
-    text: number;
-
-    constructor(theID: number, theSize: number, theText: number) {
+    isChecked: boolean = false;
+    isObstacle: boolean = false;
+    text: string;
+    cords: cordsx;
+    constructor(theID: number, theSize: number, theText: number, theCordX: number, theCordY: number) {
         this.id = theID;
         this.size = theSize;
-        this.text = theText;
+        this.text = (theText).toString();
+        this.cords = { x: theCordX, y: theCordY }
+
+
     }
 
     create(): HTMLDivElement {
         let element = document.createElement("div")
-        element.classList.add("punkt")
+        element.id = (this.id).toString()
+        element.classList.add("point")
         element.innerText = (this.text).toString()
         element.setAttribute('style', `width: ${this.size}px; height: ${this.size}px; border:1px solid black`);
         return element;
     }
+
+    checkIsAvailable() {
+        let element = document.getElementById(this.id.toString())
+        if (element != (undefined || null))
+            return element
+        else
+            return null
+    }
+    setObstacle() {
+        let obstacleElem = this.checkIsAvailable()
+        if (obstacleElem) {
+            this.isObstacle = true
+            this.text = "X"
+            obstacleElem.innerHTML = this.text
+        }
+    }
+
+    setAsStartEnd(name: string) {
+        let obstacleElem = this.checkIsAvailable()
+        if (obstacleElem) {
+            if (name == "START") {
+                this.text = "START"
+            }
+            else {
+                this.text = "END"
+            }
+            obstacleElem.style.backgroundColor = "red"
+            obstacleElem.innerHTML = this.text
+
+        }
+
+    }
 }
+
 
 class SquareList {
 
     quantity: number;
     sizeOfSquare: number;
-    squareList: Array<{
-        x: number,
-        y: number,
-        object: Object
-    }
-    >;
+    squareList: Square[]
+    startEndObject: Number[]
+
 
     constructor(theQuantity: number, theSizeOfSquare: number) {
         this.quantity = theQuantity;
         this.sizeOfSquare = theSizeOfSquare;
+        this.startEndObject = []
         this.squareList = []
-        // console.log(this.squareList)
-        // let s = new Square(0, 12)
-        // this.squareList.push({ x: 2, y: 3, object: s })
         this.makeBoard()
+        this.addListener()
     }
 
     makeBoard() {
+        let count = 0;
         for (let i = 0; i < this.quantity; i++) {
             for (let j = 0; j < this.quantity; j++) {
-                let square = new Square(i, this.sizeOfSquare, 0)
-                this.squareList.push({ x: j, y: i, object: square })
+                let square = new Square(count++, this.sizeOfSquare, 0, i, j)
+                this.squareList.push(square)
                 document.getElementById('root')?.appendChild(square.create())
             }
         }
     }
 
-    // refreshBoard() {
-    //     document.getElementById("root")?.innerHTML = ""
-    // }
+    addListener() {
+        document.addEventListener('click', (e) => {
+            if ((e.target as Element).classList.contains('point') && this.startEndObject.length < 2) {
+                let id = parseInt((e.target as Element).id)
+                if (this.startEndObject.length < 1)
+                    this.squareList.find(element => element.id == id)?.setAsStartEnd("START")
+                else
+                    this.squareList.find(element => element.id == id)?.setAsStartEnd("END")
+                this.startEndObject.push(id)
+                console.log(this.startEndObject)
+            }
+        });
+
+    }
+
 
     generateObstacles(numberOfObstacle: number) {
-        let x = ["zuzia", "kasia", "asia", "basak"]
         for (let i = 0; i < numberOfObstacle; i++) {
-            console.log("TO:")
+            let x: number = generateRandomInteger(0, 4)
+            let y: number = generateRandomInteger(0, 4)
 
-            console.log(x.find(el => el == "zuzia"))
+            this.squareList.find(element => element.cords.x == x && element.cords.y == y)?.setObstacle()
+
+            console.log(this.squareList.find(element => element.cords.x == x && element.cords.y == y))
+
         }
     }
 
