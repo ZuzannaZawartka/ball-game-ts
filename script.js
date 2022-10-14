9a27fb8e50047745a90a74a35d86c1764d6adf96
Zuzia <raf.zuzia7@gmail.com>
2022-10-14 19:02:03 +0200

    firstCommit

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-// interface startEnd {
-//     start: number | null,
-//     end: number | null;
-// }
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Square = /** @class */ (function () {
     function Square(theID, theSize, theText, theCordX, theCordY) {
         this.isChecked = false;
@@ -28,29 +39,41 @@ var Square = /** @class */ (function () {
     };
     Square.prototype.setObstacle = function () {
         var obstacleElem = this.checkIsAvailable();
-        if (obstacleElem) {
+        if (obstacleElem && !this.isObstacle && !this.isChecked) {
             this.isObstacle = true;
             this.text = "X";
             obstacleElem.innerHTML = this.text;
+            return true;
         }
+        return false;
     };
     Square.prototype.setAsStartEnd = function (name) {
         var obstacleElem = this.checkIsAvailable();
-        if (obstacleElem) {
-            if (name == "START") {
-                this.text = "START";
+        if (obstacleElem && !this.isObstacle && !this.isChecked) {
+            if (name == "START" || name == "END") {
+                this.text = name;
+                obstacleElem.style.backgroundColor = "red";
+                obstacleElem.innerHTML = this.text;
+                this.isChecked = true;
+                return true;
             }
-            else {
-                this.text = "END";
-            }
-            obstacleElem.style.backgroundColor = "red";
-            obstacleElem.innerHTML = this.text;
+            return false;
         }
     };
     return Square;
 }());
 var SquareList = /** @class */ (function () {
     function SquareList(theQuantity, theSizeOfSquare) {
+        var _this = this;
+        this.elementFinder = function (arr, target) {
+            var element = _this.squareList[0][0];
+            arr.forEach(function (row, i) {
+                var elementInRow = row.find(function (element) { return element.id == target; });
+                if (elementInRow)
+                    element = elementInRow;
+            });
+            return element;
+        };
         this.quantity = theQuantity;
         this.sizeOfSquare = theSizeOfSquare;
         this.startEndObject = [];
@@ -62,39 +85,46 @@ var SquareList = /** @class */ (function () {
         var _a;
         var count = 0;
         for (var i = 0; i < this.quantity; i++) {
+            this.squareList[i] = [];
             for (var j = 0; j < this.quantity; j++) {
                 var square = new Square(count++, this.sizeOfSquare, 0, i, j);
-                this.squareList.push(square);
+                this.squareList[i][j] = square;
                 (_a = document.getElementById('root')) === null || _a === void 0 ? void 0 : _a.appendChild(square.create());
             }
         }
+        console.log(this.squareList);
     };
     SquareList.prototype.addListener = function () {
         var _this = this;
         document.addEventListener('click', function (e) {
             var _a, _b;
             if (e.target.classList.contains('point') && _this.startEndObject.length < 2) {
-                var id_1 = parseInt(e.target.id);
-                if (_this.startEndObject.length < 1)
-                    (_a = _this.squareList.find(function (element) { return element.id == id_1; })) === null || _a === void 0 ? void 0 : _a.setAsStartEnd("START");
-                else
-                    (_b = _this.squareList.find(function (element) { return element.id == id_1; })) === null || _b === void 0 ? void 0 : _b.setAsStartEnd("END");
-                _this.startEndObject.push(id_1);
+                var id = parseInt(e.target.id);
+                if (_this.startEndObject.length < 1) {
+                    if ((_a = _this.elementFinder(_this.squareList, id)) === null || _a === void 0 ? void 0 : _a.setAsStartEnd("START"))
+                        _this.startEndObject.push(id);
+                }
+                else if ((_b = _this.elementFinder(_this.squareList, id)) === null || _b === void 0 ? void 0 : _b.setAsStartEnd("END")) {
+                    _this.startEndObject.push(id);
+                }
+                // this.squareList.find(element => element.id == id)?.setAsStartEnd("END")
                 console.log(_this.startEndObject);
             }
         });
     };
     SquareList.prototype.generateObstacles = function (numberOfObstacle) {
-        var _a;
-        var _loop_1 = function (i) {
-            var x = generateRandomInteger(0, 4);
-            var y = generateRandomInteger(0, 4);
-            (_a = this_1.squareList.find(function (element) { return element.cords.x == x && element.cords.y == y; })) === null || _a === void 0 ? void 0 : _a.setObstacle();
-            console.log(this_1.squareList.find(function (element) { return element.cords.x == x && element.cords.y == y; }));
-        };
-        var this_1 = this;
+        var _a, _b;
         for (var i = 0; i < numberOfObstacle; i++) {
-            _loop_1(i);
+            var id = generateRandomInteger(0, this.squareList.length * this.squareList[0].length);
+            console.log(id);
+            var isAvailableObs = (_a = this.elementFinder(this.squareList, id)) === null || _a === void 0 ? void 0 : _a.setObstacle();
+            console.log(isAvailableObs);
+            while (!isAvailableObs) {
+                id = generateRandomInteger(0, this.squareList.length * this.squareList[0].length);
+                console.log(id);
+                isAvailableObs = (_b = this.elementFinder(this.squareList, id)) === null || _b === void 0 ? void 0 : _b.setObstacle();
+                console.log("Losowoanie ponowne");
+            }
         }
     };
     return SquareList;
@@ -102,5 +132,63 @@ var SquareList = /** @class */ (function () {
 function generateRandomInteger(min, max) {
     return Math.floor(min + Math.random() * (max - min + 1));
 }
+var PathFinding = /** @class */ (function () {
+    function PathFinding(rows, cols) {
+        this.unevaluatedGPoints = [];
+        this.evaluatedGPoints = [];
+        this.path = [];
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = [];
+    }
+    PathFinding.prototype.heuristic = function (positionA, positionB) {
+        var d1 = Math.abs(positionA.x - positionB.x);
+        var d2 = Math.abs(positionA.y - positionB.y);
+        return d1 + d2;
+    };
+    PathFinding.prototype.pathInit = function () {
+        for (var i = 0; i < this.cols; i++) {
+            this.grid[i] = [];
+            for (var j = 0; j < this.rows; j++) {
+                this.grid[i][j] = new GridPoint(i, j);
+            }
+        }
+    };
+    PathFinding.prototype.pathFind = function () {
+    };
+    return PathFinding;
+}());
+var GridPoint = /** @class */ (function (_super) {
+    __extends(GridPoint, _super);
+    function GridPoint(cords) {
+        var _this = _super.call(this, 5, 5) || this;
+        _this.parent = undefined; //popraw typy
+        _this.x = cords.x;
+        _this.y = cords.y;
+        _this.finalCost = 0;
+        _this.toGridPointCost = 0;
+        _this.heuristicCost = 0;
+        _this.neighbors = any[];
+        _this.parent = undefined;
+        return _this;
+    }
+    GridPoint.prototype.updateNeighbors = function () {
+        var i = this.x;
+        var j = this.y;
+        if (i < this.cols - 1) {
+            this.neighbors.push(this.grid[i + 1][j]);
+        }
+        if (i > 0) {
+            this.neighbors.push(this.grid[i - 1][j]);
+        }
+        if (j < this.rows - 1) {
+            this.neighbors.push(this.grid[i][j + 1]);
+        }
+        if (j > 0) {
+            this.neighbors.push(grid[i][j - 1]);
+        }
+    };
+    return GridPoint;
+}(PathFinding));
 var x2 = new SquareList(5, 50);
-x2.generateObstacles(2);
+x2.generateObstacles(12);
--- script.ts
+++ script.ts
@@ -3,10 +3,6 @@ interface cordsx {
     y: number
 }
 
-// interface startEnd {
-//     start: number | null,
-//     end: number | null;
-// }
 class Square {
     id: number;
     size: number;
@@ -41,27 +37,27 @@ class Square {
     }
     setObstacle() {
         let obstacleElem = this.checkIsAvailable()
-        if (obstacleElem) {
+        if (obstacleElem && !this.isObstacle && !this.isChecked) {
             this.isObstacle = true
             this.text = "X"
             obstacleElem.innerHTML = this.text
+            return true
         }
+        return false
     }
 
     setAsStartEnd(name: string) {
         let obstacleElem = this.checkIsAvailable()
-        if (obstacleElem) {
-            if (name == "START") {
-                this.text = "START"
+        if (obstacleElem && !this.isObstacle && !this.isChecked) {
+            if (name == "START" || name == "END") {
+                this.text = name
+                obstacleElem.style.backgroundColor = "red"
+                obstacleElem.innerHTML = this.text
+                this.isChecked = true
+                return true
             }
-            else {
-                this.text = "END"
-            }
-            obstacleElem.style.backgroundColor = "red"
-            obstacleElem.innerHTML = this.text
-
+            return false
         }
-
     }
 }
 
@@ -70,7 +66,7 @@ class SquareList {
 
     quantity: number;
     sizeOfSquare: number;
-    squareList: Square[]
+    squareList: Square[][]
     startEndObject: Number[]
 
 
@@ -86,39 +82,58 @@ class SquareList {
     makeBoard() {
         let count = 0;
         for (let i = 0; i < this.quantity; i++) {
+            this.squareList[i] = []
             for (let j = 0; j < this.quantity; j++) {
                 let square = new Square(count++, this.sizeOfSquare, 0, i, j)
-                this.squareList.push(square)
+                this.squareList[i][j] = square
                 document.getElementById('root')?.appendChild(square.create())
             }
+
         }
+        console.log(this.squareList)
     }
 
     addListener() {
         document.addEventListener('click', (e) => {
             if ((e.target as Element).classList.contains('point') && this.startEndObject.length < 2) {
                 let id = parseInt((e.target as Element).id)
-                if (this.startEndObject.length < 1)
-                    this.squareList.find(element => element.id == id)?.setAsStartEnd("START")
+                if (this.startEndObject.length < 1) {
+                    if (this.elementFinder(this.squareList, id)?.setAsStartEnd("START"))
+                        this.startEndObject.push(id)
+                }
                 else
-                    this.squareList.find(element => element.id == id)?.setAsStartEnd("END")
-                this.startEndObject.push(id)
+                    if (this.elementFinder(this.squareList, id)?.setAsStartEnd("END")) {
+                        this.startEndObject.push(id)
+                    }
+                // this.squareList.find(element => element.id == id)?.setAsStartEnd("END")
                 console.log(this.startEndObject)
             }
         });
 
     }
 
+    elementFinder = (arr: Array<any>, target: number) => {
+        let element = this.squareList[0][0];
+        arr.forEach((row, i) => {
+            let elementInRow: Square | null = row.find((element: { id: number; }) => element.id == target)
+            if (elementInRow)
+                element = elementInRow
+        })
+        return element;
+    }
 
     generateObstacles(numberOfObstacle: number) {
         for (let i = 0; i < numberOfObstacle; i++) {
-            let x: number = generateRandomInteger(0, 4)
-            let y: number = generateRandomInteger(0, 4)
-
-            this.squareList.find(element => element.cords.x == x && element.cords.y == y)?.setObstacle()
-
-            console.log(this.squareList.find(element => element.cords.x == x && element.cords.y == y))
-
+            let id: number = generateRandomInteger(0, this.squareList.length * this.squareList[0].length)
+            console.log(id)
+            let isAvailableObs = this.elementFinder(this.squareList, id)?.setObstacle()
+            console.log(isAvailableObs)
+            while (!isAvailableObs) {
+                id = generateRandomInteger(0, this.squareList.length * this.squareList[0].length)
+                console.log(id)
+                isAvailableObs = this.elementFinder(this.squareList, id)?.setObstacle()
+                console.log("Losowoanie ponowne")
+            }
         }
     }
 
@@ -127,6 +142,88 @@ function generateRandomInteger(min: number, max: number) {
     return Math.floor(min + Math.random() * (max - min + 1))
 }
 
+
+class PathFinding {
+
+
+    rows: number
+    cols: number
+    grid: number[][]; // all grid points
+    unevaluatedGPoints = []
+    evaluatedGPoints = []
+    path = []
+
+    constructor(rows: number, cols: number) {
+        this.rows = rows
+        this.cols = cols
+        this.grid = []
+    }
+
+    heuristic(positionA: { x: number, y: number }, positionB: { x: number, y: number }) {
+        let d1 = Math.abs(positionA.x - positionB.x);
+        let d2 = Math.abs(positionA.y - positionB.y);
+        return d1 + d2
+    }
+
+
+    pathInit() {
+        for (let i = 0; i < this.cols; i++) {
+            this.grid[i] = []
+            for (let j = 0; j < this.rows; j++) {
+                this.grid[i][j] = new GridPoint(i, j);
+            }
+        }
+    }
+
+
+    pathFind() {
+
+    }
+}
+
+
+class GridPoint extends PathFinding {
+
+    x: number;
+    y: number;
+    finalCost: number; //total cost
+    toGridPointCost: number; // from start to current grid point
+    heuristicCost: number;
+    neighbors: []; //poprawy typy
+    parent = undefined; //popraw typy
+
+    constructor(cords: { x: number, y: number }) {
+        super(5, 5)
+        this.x = cords.x
+        this.y = cords.y
+        this.finalCost = 0
+        this.toGridPointCost = 0
+        this.heuristicCost = 0
+        this.neighbors = any[]
+        this.parent = undefined
+    }
+
+    updateNeighbors() {
+        let i = this.x
+        let j = this.y
+        if (i < this.cols - 1) {
+            this.neighbors.push(this.grid[i + 1][j]);
+        }
+        if (i > 0) {
+            this.neighbors.push(this.grid[i - 1][j]);
+        }
+        if (j < this.rows - 1) {
+            this.neighbors.push(this.grid[i][j + 1]);
+        }
+        if (j > 0) {
+            this.neighbors.push(grid[i][j - 1]);
+        }
+    }
+
+
+
+}
+
 let x2 = new SquareList(5, 50)
-x2.generateObstacles(2)
+x2.generateObstacles(12)
 
